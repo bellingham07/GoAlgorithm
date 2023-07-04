2b7d47b22e88da02645023449896903063f2659b
bellingham <75364055@qq.com>
2023-07-04 09:48:47 +0800

    sort

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -1,17 +1,13 @@
 package sort
 
 import (
-	"fmt"
 	"testing"
 )
 
 var nums = []int{9, 5, 3, 6, 8, 1, 2, 5, 4, 7, 8}
 
 func TestSort(t *testing.T) {
-	fmt.Println("bubble sort")
-	BubbleSort(nums)
-	fmt.Println("select sort")
-	SelectSort(nums)
-	fmt.Println("insert sort")
+	//BubbleSort(nums)
+	//SelectSort(nums)
 	InsertSort(nums)
 }
