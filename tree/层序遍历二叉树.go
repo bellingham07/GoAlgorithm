ff8bf5971186480935c4c8a53d6a775b5958abef
bellingham <75364055@qq.com>
2023-08-05 09:49:21 +0800

    层序遍历二叉树

---
+++ tree/层序遍历二叉树.go
@@ -0,0 +1,20 @@
+package tree
+
+func levelOrder(root *TreeNode) [][]int {
+	arr := [][]int{}
+	depth := 0
+	var order func(root *TreeNode, depth int)
+	order = func(root *TreeNode, depth int) {
+		if root == nil {
+			return
+		}
+		if len(arr) == depth {
+			arr = append(arr, []int{})
+		}
+		arr[depth] = append(arr[depth], root.Val)
+		order(root.Left, depth+1)
+		order(root.Right, depth+1)
+	}
+	order(root, depth)
+	return arr
+}
